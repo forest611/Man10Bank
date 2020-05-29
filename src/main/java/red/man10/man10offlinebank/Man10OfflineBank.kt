b476aab5d2413f976a8722cda49ea85bc8faf71f
forest611 <forest611328@gmail.com>
2020-05-29 11:36:49 +0900

    Initial commit

---
+++ src/main/java/red/man10/man10offlinebank/Man10OfflineBank.kt
@@ -0,0 +1,13 @@
+package red.man10.man10offlinebank
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Man10OfflineBank : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
