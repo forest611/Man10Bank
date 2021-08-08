645fbb8b328a01e375d3279a6be0f919265e18cf
forest611 <forest611328@gmail.com>
2021-08-08 14:46:15 +0900

    運営用のコマンド追加

--- src/main/java/red/man10/man10bank/loan/ServerLoan.kt
+++ src/main/java/red/man10/man10bank/loan/ServerLoan.kt
@@ -1,6 +1,7 @@
 package red.man10.man10bank.loan
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.event.ClickEvent
 import org.bukkit.Bukkit
 import org.bukkit.entity.Player
 import red.man10.man10bank.Man10Bank
@@ -9,21 +10,24 @@ import red.man10.man10bank.Man10Bank.Companion.plugin
 import red.man10.man10bank.Man10Bank.Companion.sendMsg
 import red.man10.man10bank.MySQLManager
 import red.man10.man10score.ScoreDatabase
+import java.util.concurrent.ConcurrentHashMap
 
 object ServerLoan {
 
-    private val mysql = MySQLManager(Man10Bank.plugin,"Man10ServerLoan")
+    private val mysql = MySQLManager(plugin,"Man10ServerLoan")
 
     var scoreMultiplier = 1.0//スコアの乗数
     var recordMultiplier = 1.0//レコード数の乗数
     var medianMultiplier = 1.0//中央値の乗数
 
+    val shareMap = ConcurrentHashMap<Player,Double>()
+
     var maxServerLoanAmount = 1_000_000.0
 
     fun checkServerLoan(p: Player){
 
-//        val score = ScoreDatabase.getScore(p.uniqueId)
-        val score = 10
+        val score = ScoreDatabase.getScore(p.uniqueId)
+//        val score = 10
 
         val list = mutableListOf<Double>()
 
@@ -71,17 +75,70 @@ object ServerLoan {
 
         broadcastOnMainThread("§e§l[まんじゅう銀行AI]§7計算完了！")
 
-        Bukkit.getLogger().info("m:${m}")
+        Thread.sleep(1000)
 
         Bukkit.getLogger().info("スコア乗数${scoreMultiplier}現在のスコア${score}")
         Bukkit.getLogger().info("レコード数の乗数${recordMultiplier}レコード数の合計${records}")
         Bukkit.getLogger().info("中央値の乗数${medianMultiplier}中央値${median}")
 
-        val maxServerLoan = median * medianMultiplier * score* scoreMultiplier * records * recordMultiplier
+        val calcAmount = median * medianMultiplier * score* scoreMultiplier * records * recordMultiplier
+        val maxLoan = if (maxServerLoanAmount<calcAmount) maxServerLoanAmount else calcAmount
 
-        Bukkit.getLogger().info("MaxServerLoan:${format(maxServerLoan)}")
+        Bukkit.getLogger().info("MaxServerLoan:${format(maxLoan)}")
 
-        p.sendMessage("貸し出し可能上限額:${format(if (maxServerLoanAmount<maxServerLoan) maxServerLoanAmount else maxServerLoan)}")
+        p.sendMessage("§f§l貸し出し可能上限額:§e§l${format(maxLoan)}円(最大:${format(maxServerLoanAmount)}円)")
+
+        p.sendMessage(Component.text("§e§l§n[結果をシェアする]").clickEvent(ClickEvent.runCommand("/slend share")))
+
+        shareMap[p] = maxLoan
+    }
+
+    fun checkServerLoan(sender:Player,p:Player){
+
+        val score = ScoreDatabase.getScore(p.uniqueId)
+//        val score = 10
+
+        val list = mutableListOf<Double>()
+
+        val rs = mysql.query("select total from estate_history_tbl where uuid='${p.uniqueId}';")?:return
+
+        while (rs.next()){
+            list.add(rs.getDouble("total"))
+        }
+
+        rs.close()
+        mysql.close()
+
+        val rs2 = mysql.query("select count(*) from estate_history_tbl where uuid='${p.uniqueId}';")?:return
+
+        val records = if (rs2.next())rs2.getInt(1) else 0
+
+        list.sort()
+        val m = list.size/2
+
+        val median : Double = if (list.size%2 == 0){
+            (list[m]+list[m+1])/2.0
+        }else{
+            list[m]+1
+        }
+
+
+        sender.sendMessage("スコア乗数${scoreMultiplier}現在のスコア${score}")
+        sender.sendMessage("レコード数の乗数${recordMultiplier}レコード数の合計${records}")
+        sender.sendMessage("中央値の乗数${medianMultiplier}中央値${median}")
+
+        val calcAmount = median * medianMultiplier * score* scoreMultiplier * records * recordMultiplier
+        val maxLoan = if (maxServerLoanAmount<calcAmount) maxServerLoanAmount else calcAmount
+
+        Bukkit.getLogger().info("MaxServerLoan:${format(maxLoan)}")
+
+        sender.sendMessage("§f§l貸し出し可能上限額:§e§l${format(maxLoan)}円(最大:${format(maxServerLoanAmount)}円)")
+
+//        p.sendMessage(Component.text("§e§l§n[結果をシェアする]").clickEvent(ClickEvent.runCommand("/slend share")))
+
+//        shareMap[p] = maxLoan
+
+
     }
 
     private fun broadcastOnMainThread(msg:String){
--- src/main/java/red/man10/man10bank/loan/ServerLoanCommand.kt
+++ src/main/java/red/man10/man10bank/loan/ServerLoanCommand.kt
@@ -1,10 +1,17 @@
 package red.man10.man10bank.loan
 
+import net.kyori.adventure.text.Component
+import org.bukkit.Bukkit
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import red.man10.man10bank.Man10Bank
+import red.man10.man10bank.Man10Bank.Companion.OP
+import red.man10.man10bank.Man10Bank.Companion.es
+import red.man10.man10bank.Man10Bank.Companion.format
+import red.man10.man10bank.Man10Bank.Companion.prefix
+import red.man10.man10bank.Man10Bank.Companion.sendMsg
 
 class ServerLoanCommand : CommandExecutor{
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
@@ -22,10 +29,42 @@ class ServerLoanCommand : CommandExecutor{
 
         when(args[0]){
 
-            "check" ->{
-                Man10Bank.es.execute {
-                    ServerLoan.checkServerLoan(sender)
+//            "check" ->{
+//                es.execute {
+//                    ServerLoan.checkServerLoan(sender)
+//                }
+//            }
+
+            "checkop" ->{
+
+                if (!sender.hasPermission(OP))return true
+
+                es.execute {
+
+                    val p = Bukkit.getPlayer(args[1])
+
+                    if (p==null){
+                        sendMsg(sender,"ユーザーがオフラインです")
+                        return@execute
+                    }
+
+                    ServerLoan.checkServerLoan(sender,p)
                 }
+            }
+
+            "share" ->{
+
+                val amount = ServerLoan.shareMap[sender]?:-1.0
+
+                if (amount == -1.0){
+                    sender.sendMessage("あなたは貸し出し可能金額の審査をしておりません！")
+                    return true
+                }
+
+                Bukkit.broadcast(Component.text("${prefix}§b§l${sender.name}§a§lさんの公的ローン貸し出し可能金額は" +
+                        "・・・§e§l${format(amount)}円§a§lです！"))
+
+                ServerLoan.shareMap.remove(sender)
             }
 
 
