82d295f2cf1491eac751f2aec804a231c5662208
forest611 <forest611328@gmail.com>
2023-02-21 16:25:13 +0900

    init commit

---
+++ Man10Bank/src/main/java/red/man10/man10bank/Man10Bank.java
@@ -0,0 +1,17 @@
+package red.man10.man10bank;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Man10Bank extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
