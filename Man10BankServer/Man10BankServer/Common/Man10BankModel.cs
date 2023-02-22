9632872927342633c1d6dadea25ebbee98147a94
forest611 <forest611328@gmail.com>
2023-02-22 15:03:46 +0900

    EFCoreを使うためのおまじないを書いた

--- Man10BankServer/Man10BankServer/Common/Man10BankModel.cs
+++ Man10BankServer/Man10BankServer/Common/Man10BankModel.cs
@@ -16,6 +16,9 @@ public class UserBank
 
 }
 
+/// <summary>
+/// 銀行ログのテーブル
+/// </summary>
 public class MoneyLog
 {
     
@@ -30,16 +33,149 @@ public class MoneyLog
     public string server { get; set; }
     public bool deposit { get; set; }
     public DateTime date { get; set; }
-    
-    
+}
+
+/// <summary>
+/// ATMのログ
+/// </summary>
+public class ATMLog
+{
+    [Key]
+    public int id { get; set; }
+    public string player { get; set; }
+    public string uuid { get; set; }
+    public double amount { get; set; }
+    public bool deposit { get; set; }
+    public DateTime date { get; set; }
+}
+
+/// <summary>
+/// ユーザー間借金のテーブル
+/// </summary>
+public class LoanTable
+{
+    [Key]
+    public int id { get; set; }
+    public string lend_player { get; set; }
+    public string lend_uuid { get; set; }
+    public string borrow_player { get; set; }
+    public string borrow_uuid { get; set; }
+    public DateTime borrow_date { get; set; }
+    public DateTime payback_date { get; set; }
+    public double amount { get; set; }
+}
+
+/// <summary>
+/// 最新の資産テーブル
+/// </summary>
+public class EstateTable
+{
+    [Key]
+    public int id { get; set; }
+    public string player { get; set; }
+    public string uuid { get; set; }
+    public DateTime date { get; set; }
+    public double vault { get; set; }
+    public double bank { get; set; }
+    public double cash { get; set; }
+    public double estate { get; set; }
+    public double loan { get; set; }
+    public double shop { get; set; }
+    public double crypto { get; set; }
+    public double total { get; set; }
+}
+
+/// <summary>
+/// 資産履歴テーブル
+/// </summary>
+public class EstateHistoryTable
+{
+    [Key]
+    public int id { get; set; }
+    public string player { get; set; }
+    public string uuid { get; set; }
+    public DateTime date { get; set; }
+    public double vault { get; set; }
+    public double bank { get; set; }
+    public double cash { get; set; }
+    public double estate { get; set; }
+    public double loan { get; set; }
+    public double shop { get; set; }
+    public double crypto { get; set; }
+    public double total { get; set; }
+}
+
+/// <summary>
+/// サーバー全体の資産ヒストリー
+/// </summary>
+public class ServerEstateHistory
+{
+    [Key]
+    public int id { get; set; }
+    public double vault { get; set; }
+    public double bank { get; set; }
+    public double cash { get; set; }
+    public double estate { get; set; }
+    public double loan { get; set; }
+    public double shop { get; set; }
+    public double crypto { get; set; }
+    public double total { get; set; }
+    public int year { get; set; }
+    public int month { get; set; }
+    public int day { get; set; }
+    public int hour { get; set; }
+    public DateTime date { get; set; }
     
 }
+
+/// <summary>
+/// 小切手のテーブル
+/// </summary>
+public class ChequeTable
+{
+    [Key]
+    public int id { get; set; }
+    public string player { get; set; }
+    public string uuid { get; set; }
+    public double amount { get; set; }
+    public string note { get; set; }
+    public DateTime date { get; set; }
+    public DateTime use_date { get; set; }
+    public bool used { get; set; }
+}
+
+
+/// <summary>
+/// リボのテーブル
+/// </summary>
+public class ServerLoanTable
+{
+    [Key]
+    public int id { get; set; }
+    public string player { get; set; }
+    public string uuid { get; set; }
+    public DateTime borrow_date { get; set; }
+    public DateTime last_pay_date { get; set; }
+    public double borrow_amount { get; set; }
+    public double payment_amount { get; set; }
+    public int failed_payment { get; set; }
+    public bool stop_interest { get; set; }
+}
+
 public class Context : DbContext
 {
     
     public DbSet<UserBank> user_bank { get; set; }
     public DbSet<MoneyLog> money_log { get; set; }
-    
+    public DbSet<ATMLog> atm_log { get; set; }
+    public DbSet<LoanTable> loan_table { get; set; }
+    public DbSet<EstateTable> estate_tbl { get; set; }
+    public DbSet<EstateHistoryTable> estate_history_tbl { get; set; }
+    public DbSet<ServerEstateHistory> server_estate_history { get; set; }
+    public DbSet<ChequeTable> cheque_tbl { get; set; }
+    public DbSet<ServerLoanTable> server_loan_tbl { get; set; }
+
+
     private static string Host { get; }
     private static string Port { get; }
     private static string Pass { get; }
---
+++ Man10BankServer/Man10BankServer/db_tables.sql
@@ -0,0 +1,166 @@
+create table user_bank
+(
+    id int auto_increment,
+    player varchar(16) not null,
+    uuid varchar(36) not null,
+    balance double default 0.0 not null,
+    constraint user_bank_pk
+        primary key (id)
+);
+
+create index user_bank_id_uuid_player_index
+    on user_bank (id, uuid, player);
+
+create table money_log
+(
+    id int auto_increment,
+    player varchar(16) not null,
+    uuid varchar(36) not null,
+    plugin_name varchar(16) null,
+    amount double default 0 not null,
+    note varchar(64) null,
+    display_note varchar(64) null,
+    server varchar(16) null,
+    deposit boolean default true null,
+    date datetime default now() not null,
+    constraint money_log_pk
+        primary key (id)
+);
+
+create index money_log_id_uuid_player_index
+    on money_log (id, uuid, player);
+
+create table loan_table
+(
+    id int auto_increment,
+    lend_player varchar(16) null,
+    lend_uuid varchar(36) null,
+    borrow_player varchar(16) null,
+    borrow_uuid varchar(36) null,
+    borrow_date datetime default now() null,
+    payback_date datetime null,
+    amount double default 0.0 not null,
+    constraint loan_table_pk
+        primary key (id)
+);
+
+create index loan_table_player_uuid_index
+    on loan_table (borrow_player, borrow_uuid);
+
+create table atm_log
+(
+    id int auto_increment,
+    player varchar(16) null,
+    uuid varchar(36) null,
+    amount double null,
+    deposit boolean null,
+    date datetime default now() null,
+    constraint atm_log_pk
+        primary key (id)
+);
+
+create table estate_tbl
+(
+    id int auto_increment,
+    uuid varchar(36) not null,
+    player varchar(16) null,
+    date datetime null,
+    vault double default 0 null,
+    bank double default 0 null,
+    cash double default 0 null,
+    estate double default 0 null,
+    loan double default 0 null,
+    shop double default 0 null,
+    crypto double default 0 null,
+    total double default 0 null,
+    constraint estate_tbl_pk
+        primary key (id)
+)
+    comment '現在の個人の資産テーブル';
+
+create index estate_tbl_uuid_index
+    on estate_tbl (uuid);
+
+
+create table estate_history_tbl
+(
+    id int auto_increment,
+    uuid varchar(36) null,
+    player varchar(16) null,
+    date datetime default now() null,
+    vault double default 0 null,
+    bank double default 0 null,
+    cash double default 0 null,
+    estate double default 0 null,
+    loan double default 0 null,
+    shop double default 0 null,
+    crypto double default 0 null,
+    total double default 0 null,
+    constraint estate_history_tbl_pk
+        primary key (id)
+);
+
+create index estate_history_tbl_uuid_index
+    on estate_history_tbl (uuid);
+
+
+create table server_estate_history
+(
+    id int auto_increment,
+    vault double null,
+    bank double default 0 null,
+    cash double default 0 null,
+    estate double default 0 null,
+    loan double default 0 null,
+    crypto double default 0 null,
+    shop double default 0 null,
+    total double default 0 null,
+    year int null,
+    month int null,
+    day int null,
+    hour int null,
+    date datetime default now() null,
+    constraint server_estate_history_pk
+        primary key (id)
+);
+
+create index server_estate_history_year_month_day_hour_index
+    on server_estate_history (year, month, day, hour);
+
+create table cheque_tbl
+(
+    id int auto_increment,
+    player varchar(16) null,
+    uuid varchar(36) null,
+    amount double null,
+    note varchar(128) null,
+    date datetime null,
+    use_date datetime null,
+    used tinyint default 0 null,
+    constraint cheque_tbl_pk
+        primary key (id)
+);
+
+create index cheque_tbl_used_index
+    on cheque_tbl (used);
+
+create table server_loan_tbl
+(
+    id int auto_increment,
+    player varchar(16) null comment '借りたプレイヤー',
+    uuid varchar(36) null,
+    borrow_date datetime default now() null comment '借りた日
+',
+    last_pay_date datetime default now() null comment '最後に支払った日
+',
+    borrow_amount double null comment '借りた金額の合計',
+    payment_amount double null comment '週ごとの支払額',
+    failed_payment int default 0 comment '支払いに失敗した回数',
+    stop_interest tinyint default 0 comment '利息をたすかどうか'
+        constraint server_loan_tbl_pk
+        primary key (id)
+);
+
+create index server_loan_tbl_uuid_borrow_amount_index
+    on server_loan_tbl (uuid, borrow_amount);
+
