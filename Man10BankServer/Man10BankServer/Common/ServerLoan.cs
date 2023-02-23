d0207cf27a82f784a7243ab85f08360f736000bc
forest611 <forest611328@gmail.com>
2023-02-23 17:30:07 +0900

    リボの処理書き始め

--- Man10BankServer/Man10BankServer/Common/ServerLoan.cs
+++ Man10BankServer/Man10BankServer/Common/ServerLoan.cs
@@ -1,6 +1,29 @@
 namespace Man10BankServer.Common;
 
-public class ServerLoan
+public static class ServerLoan
 {
+
+    
+    public static double CalculateLoanAmount(string uuid)
+    {
+
+        return 0.0;
+    }
+
+    public static double GetBorrowingInfo(string uuid)
+    {
+        
+        return 0.0;
+    }
+
+    public static void SetBorrowingInfo()
+    {
+        
+    }
+    
+    
+    
+    
+    
     
 }
