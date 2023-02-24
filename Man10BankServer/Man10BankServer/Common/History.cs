1ab3aa7172b600375bd6f5f9271318ecc66c82f6
forest611 <forest611328@gmail.com>
2023-02-25 00:44:20 +0900

    履歴クラス作成

---
+++ Man10BankServer/Man10BankServer/Common/History.cs
@@ -0,0 +1,53 @@
+using Man10BankServer.Model;
+
+namespace Man10BankServer.Common;
+
+public static class History
+{
+
+    public static void AddServerEstateHistory()
+    {
+        var context = new Context();
+
+        var year = DateTime.Now.Year;
+        var month = DateTime.Now.Month;
+        var day = DateTime.Now.Day;
+        var hour = DateTime.Now.Hour;
+
+
+        var hasData = context.server_estate_history.Any(r =>
+            r.year == year && r.month == month && r.day == day && r.hour == hour);
+
+        if (hasData)
+        {
+            context.Dispose();
+            return;
+        }
+        
+        var estate = context.estate_tbl;
+
+        var record = new ServerEstateHistory
+        {
+            vault = estate.Sum(r => r.vault),
+            bank = estate.Sum(r => r.bank),
+            cash = estate.Sum(r => r.cash),
+            estate = estate.Sum(r => r.estate),
+            loan = estate.Sum(r => r.estate),
+            shop = estate.Sum(r=>r.shop),
+            crypto = 0,
+            date = DateTime.Now,
+            year = year,
+            month = month,
+            day = day,
+            hour = hour,
+            total = estate.Sum(r => r.total)
+        };
+
+        context.server_estate_history.Add(record);
+
+        context.Dispose();
+    }
+    
+    
+
+}
