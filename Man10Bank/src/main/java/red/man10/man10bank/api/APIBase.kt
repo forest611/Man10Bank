f49a0002e8699daf470ba6cb734641ff0f45b186
forest611 <forest611328@gmail.com>
2023-03-03 12:16:49 +0900

    接続が通った

--- Man10Bank/src/main/java/red/man10/man10bank/api/APIBase.kt
+++ Man10Bank/src/main/java/red/man10/man10bank/api/APIBase.kt
@@ -4,6 +4,13 @@ import com.google.gson.Gson
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import red.man10.man10bank.Man10Bank
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
+import java.util.concurrent.TimeUnit
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
+
 
 object APIBase {
 
@@ -14,8 +21,27 @@ object APIBase {
 
     fun setup(){
 
-        client = OkHttpClient.Builder().cache(null).build()
-        url = Man10Bank.instance.config.getString("api.url")?:"https://localhost:7031/Bank/"
+        val trustAllCerts = arrayOf<TrustManager>(
+            object : X509TrustManager {
+                override fun checkClientTrusted(chain: Array<X509Certificate>, authType: String) {}
+                override fun checkServerTrusted(chain: Array<X509Certificate>, authType: String) {}
+                override fun getAcceptedIssuers(): Array<X509Certificate> {
+                    return arrayOf()
+                }
+            }
+        )
+
+        val sslContext = SSLContext.getInstance("SSL")
+        sslContext.init(null, trustAllCerts, SecureRandom())
+
+
+        client = OkHttpClient.Builder()
+            .cache(null)
+            .readTimeout(10,TimeUnit.SECONDS)
+            .writeTimeout(10,TimeUnit.SECONDS)
+            .sslSocketFactory(sslContext.socketFactory,trustAllCerts[0] as X509TrustManager)
+            .build()
+        url = Man10Bank.instance.config.getString("api.url")?:"https://localhost:7031"
 
     }
 
