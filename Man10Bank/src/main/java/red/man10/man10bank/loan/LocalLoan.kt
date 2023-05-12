b078217b23cd87c62e90bcf76017cabd3b31067b
Jin Morikawa <forest611328@gmail.com>
2023-05-12 14:23:00 +0900

    手形コマンドなどを追加

--- Man10Bank/src/main/java/red/man10/man10bank/loan/LocalLoan.kt
+++ Man10Bank/src/main/java/red/man10/man10bank/loan/LocalLoan.kt
@@ -1,6 +1,7 @@
 package red.man10.man10bank.loan
 
 import net.kyori.adventure.text.Component.text
+import net.kyori.adventure.text.event.ClickEvent.runCommand
 import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.NamespacedKey
@@ -17,26 +18,23 @@ import red.man10.man10bank.Man10Bank.Companion.instance
 import red.man10.man10bank.Man10Bank.Companion.vault
 import red.man10.man10bank.api.APIBank
 import red.man10.man10bank.api.APILocalLoan
-import red.man10.man10bank.bank.Bank
 import red.man10.man10bank.util.BlockingQueue
-import red.man10.man10bank.util.Utility
 import red.man10.man10bank.util.Utility.format
 import red.man10.man10bank.util.Utility.msg
+import red.man10.man10bank.util.Utility.prefix
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.HashMap
 
 object LocalLoan: Listener,CommandExecutor{
 
     /**
      * 個人間借金を作る
      */
-    fun create(lender:Player,borrower:Player,amount:Double,interest:Double,paybackDay:Int){
+    private fun create(lender:Player, borrower:Player, amount:Double, interest:Double, due:Int){
 
-        val paybackDate = Calendar.getInstance()
-        paybackDate.time = Date()
-        paybackDate.add(Calendar.DAY_OF_YEAR,paybackDay)
-
-        val payment = amount*(interest+1)
+        val paybackDate = calcDue(due)
+        val payment = calcAmount(amount,interest,due)
 
         val ret = APILocalLoan.create(
             APILocalLoan.LocalLoanTable(
@@ -46,7 +44,7 @@ object LocalLoan: Listener,CommandExecutor{
             borrower.name,
             borrower.uniqueId.toString(),
             Date(),
-            paybackDate.time,
+            paybackDate,
             payment
         ))
 
@@ -61,7 +59,7 @@ object LocalLoan: Listener,CommandExecutor{
     /**
      * 手形を発行する
      */
-    fun getNote(id:Int): ItemStack? {
+    private fun getNote(id:Int): ItemStack? {
 
         val data = APILocalLoan.getInfo(id)?:return null
 
@@ -75,7 +73,7 @@ object LocalLoan: Listener,CommandExecutor{
             "§4§l========[Man10Bank]========",
             "   §7§l債務者:  ${Bukkit.getOfflinePlayer(data.borrow_uuid).name}",
             "   §8§l有効日:  ${SimpleDateFormat("yyyy-MM-dd").format(data.payback_date)}",
-            "   §7§l支払額:  ${Utility.format(data.amount)}",
+            "   §7§l支払額:  ${format(data.amount)}",
             "§4§l==========================")
 
         meta.persistentDataContainer.set(NamespacedKey(instance,"id"), PersistentDataType.INTEGER,id)
@@ -85,6 +83,16 @@ object LocalLoan: Listener,CommandExecutor{
         return note
     }
 
+    private fun calcAmount(amount: Double,interest: Double,due: Int):Double{
+        return amount + (amount * interest * due)
+    }
+
+    private fun calcDue(day:Int,borrow:Date = Date()):Date{
+        val calender = Calendar.getInstance()
+        calender.time = borrow
+        calender.add(Calendar.DAY_OF_YEAR,day)
+        return calender.time
+    }
 
     @EventHandler
     fun asyncUseNote(e:PlayerInteractEvent){
@@ -161,7 +169,6 @@ object LocalLoan: Listener,CommandExecutor{
                 }
 
                 else ->{
-
                     APIBank.addBank(
                         APIBank.TransactionData(
                             uuid.toString(),
@@ -180,9 +187,106 @@ object LocalLoan: Listener,CommandExecutor{
         }
     }
 
+    private val map = HashMap<UUID,CommandParam>()
+
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>?): Boolean {
+
+        if (label != "mlend")return true
+
+        if (sender !is Player)return true
+
+        if (args.isNullOrEmpty()){
+            msg(sender,"§a/mlend <貸す相手> <金額> <返済日(日)> <金利(日)0.0〜1.0>")
+            return true
+        }
+
+        if (args[0] == "allow"){
+            val data = map[sender.uniqueId]?:return true
+
+            val lendP = Bukkit.getPlayer(data.lender)
+
+            if (lendP == null){
+                msg(sender,"提案者がログアウトしたようです")
+                return true
+            }
+
+            BlockingQueue.addTask {
+
+                if (vault.withdraw(sender.uniqueId,data.amount)){
+                    //TODO
+                }
+
+                create(lendP,sender,data.amount,data.interest,data.due)
+            }
+
+            return true
+        }
+
+        if (args[0] == "deny"){
+            val data = map[sender.uniqueId]?:return true
+
+            val p = Bukkit.getPlayer(data.lender)
+            if (p!=null){
+                msg(p,"提案が断られました")
+            }
+
+            msg(sender,"提案を断りました")
+
+            map.remove(sender.uniqueId)
+            return true
+        }
+
+        val borrower = Bukkit.getPlayer(args[0])
+        val amount = args[1].toDoubleOrNull()
+        val due = args[2].toIntOrNull()
+        val interest = args[3].toDoubleOrNull()
+
+        if (borrower == null){
+            msg(sender,"オンラインでないプレイヤーです")
+            return true
+        }
+
+        if (amount == null){
+            msg(sender,"金額は数字で入力してください")
+            return true
+        }
+
+        if (due == null){
+            msg(sender,"期日は数字で入力してください")
+            return true
+        }
+
+        if (interest == null){
+            msg(sender,"金利は数字で入力してください")
+            return true
+        }
+
+        val data = CommandParam(sender.uniqueId,amount, due, interest)
+        map[borrower.uniqueId] = data
+
+        val allowOrDeny = text("${prefix}§b§l§n[借りる] ").clickEvent(runCommand("/mlend allow"))
+            .append(text("§c§l§n[借りない]").clickEvent(runCommand("/mlend deny")))
+
+        val sdf = SimpleDateFormat("yyyy-MM-dd")
+
+        msg(sender,"§a§l借金の提案を相手に提示しました")
+
+        msg(borrower,"§e§l＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝")
+        msg(borrower,"§e§kXX§b§l借金の提案§e§kXX")
+        msg(borrower,"§e貸し出す人:${sender.name}")
+        msg(borrower,"§e貸し出される金額:${format(amount)}")
+        msg(borrower,"§e返す金額:${format(calcAmount(amount, interest, due))}")
+        msg(borrower,"§e返す日:$${sdf.format(calcDue(due))}")
+        borrower.sendMessage(allowOrDeny)
+        msg(borrower,"§e§l＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝＝")
         return true
     }
 
+    data class CommandParam(
+        val lender: UUID,
+        val amount : Double,
+        val due : Int,
+        val interest : Double
+    )
 
 }
