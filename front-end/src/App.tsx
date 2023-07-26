f489bd52b07ca70e8d849710eaa7e7b51d3d3e0f
forest611 <forest611328@gmail.com>
2023-07-27 04:44:07 +0900

    サンプルコードを作った

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const App: React.FC = () => {
+  const [uuid, setUuid] = useState<string>('');
+  const [balance, setBalance] = useState<string | null>(null);
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value)
+    setUuid(event.target.value);
+  };
+
+  const fetchBalance = async () => {
+
+    console.log(encodeURIComponent(uuid))
+
+    try {
+      const response = await fetch(`http://cortex.jp:6110/bank/balance?uuid=${encodeURIComponent(uuid)}`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.text();
+
+      console.log(`Balance:${data}`)
+      setBalance(data);
+    } catch (error) {
+      console.error('Error fetching balance:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>Get Balance</h1>
+        <div>
+          <label htmlFor="uuidInput">Enter UUID:</label>
+          <input
+              type="text"
+              id="uuidInput"
+              value={uuid}
+              onChange={handleInputChange}
+          />
+          <button onClick={fetchBalance}>Get Balance</button>
+        </div>
+        {balance !== null ? (
+            <div>
+              <p>Balance: {balance}</p>
+            </div>
+        ) : null}
+      </div>
   );
-}
+};
 
 export default App;
