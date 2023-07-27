587adde6bfd43cf375342045875c63c42f7b22f6
forest611 <forest611328@gmail.com>
2023-07-28 00:00:30 +0900

    Configを追加

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import BalancePage from './components/BalancePage';
+import UuidPage from "./components/UuidPage";
+import EstatePage from "./components/EstatePage";
+import config from './config.json'
 
-function App() {
+export let apiUrl : string = ""
+
+const App: React.FC = () => {
+
+  loadConfig()
+
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +29,10 @@ function App() {
       </header>
     </div>
   );
+}
+
+function loadConfig() {
+  apiUrl = config.apiUrl
 }
 
 export default App;
---
+++ front-end/src/components/EstatePage.tsx
@@ -0,0 +1,56 @@
+import React, {useState} from "react";
+import {getUUID} from "../services/BankApi";
+import {EstateData, getEstate} from "../services/EstateApi";
+const EstatePage : React.FC = () => {
+
+    const [estate,setEstate] = useState<EstateData | null>(null)
+
+    const showResult = () => {
+        if (estate === null)return '存在しないユーザーです'
+        return `
+        更新日:${estate.date.toLocaleString()}\n
+        ユーザー名:${estate.player}\n
+        電子マネー:${estate.vault.toLocaleString()}円\n
+        現金:${estate.cash.toLocaleString()}円\n
+        銀行:${estate.bank.toLocaleString()}円\n
+        リボ:${estate.loan.toLocaleString()}円\n
+        総額${estate.total.toLocaleString()}円\n
+        `
+    }
+
+    return (
+        <div>
+            <h1>資産状況の確認</h1>
+            <div>
+                <label htmlFor="input">UUIDかMCIDを入力:</label>
+                <input
+                    type="text"
+                    id="input"
+                    onChange={async e => {
+                        const value = e.target.value
+
+                        if (value.length <= 3) {
+                            setEstate(null)
+                            return
+                        }
+
+                        //uuid
+                        if (value.length === 36) {
+                            setEstate(await getEstate(value))
+                            return
+                        }
+                        //mcid
+                        const uuid = await getUUID(value)
+                        if (uuid.length === 36)setEstate(await getEstate(uuid))
+
+                    }}
+                    placeholder="UUIDかMCIDを入力してください"
+                />
+                <pre>{showResult()}</pre>
+            </div>
+        </div>
+    );
+
+}
+
+export default EstatePage
