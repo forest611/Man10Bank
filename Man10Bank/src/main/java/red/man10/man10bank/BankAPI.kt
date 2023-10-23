9417f1ad1b5b25e47abd1a437ade05db31606df0
Jin Morikawa <forest611328@gmail.com>
2023-10-23 18:05:54 +0900

    プラグイン用APIの修正

--- Man10Bank/src/main/java/red/man10/man10bank/BankAPI.kt
+++ Man10Bank/src/main/java/red/man10/man10bank/BankAPI.kt
@@ -2,6 +2,7 @@ package red.man10.man10bank
 
 import org.bukkit.plugin.java.JavaPlugin
 import red.man10.man10bank.api.APIBank
+import red.man10.man10bank.api.APIServerLoan
 import java.util.*
 
 class BankAPI(private val plugin: JavaPlugin) {
@@ -24,7 +25,7 @@ class BankAPI(private val plugin: JavaPlugin) {
     /**
      * 入金リクエストを送る
      */
-    fun deposit(uuid: UUID,amount:Double,note:String,displayNote:String){
+    fun deposit(uuid: UUID,amount:Double,note:String,displayNote:String):Boolean{
         val result = APIBank.addBank(APIBank.TransactionData(
             uuid.toString(),
             amount,
@@ -33,17 +34,31 @@ class BankAPI(private val plugin: JavaPlugin) {
             displayNote
         ))
 
-//        return result == APIBank.BankResult.SUCCESSFUL
+        return result == APIBank.BankResult.SUCCESSFUL
     }
 
-    @Deprecated("displayNoteが設定できない", ReplaceWith("DisplayNoteつき"),DeprecationLevel.WARNING)
-    fun deposit(uuid: UUID,amount: Double,note: String){
-        deposit(uuid,amount, note,note)
+    /**
+     * 銀行金額を取得する
+     */
+    fun getBank(uuid: UUID):Double{
+        return APIBank.getBalance(uuid)
     }
 
-    @Deprecated("displayNoteが設定できない", ReplaceWith("DisplayNoteつき"),DeprecationLevel.WARNING)
-    fun withdraw(uuid:UUID,amount:Double,note:String):Boolean{
-        return withdraw(uuid,amount, note,note)
+    /**
+     * リボの金額を取得する
+     */
+    fun getServerLoan(uuid: UUID):Double{
+        return APIServerLoan.getInfo(uuid)?.borrow_amount?:0.0
     }
+
+//    @Deprecated("displayNoteが設定できない", ReplaceWith("DisplayNoteつき"),DeprecationLevel.WARNING)
+//    fun deposit(uuid: UUID,amount: Double,note: String){
+//        deposit(uuid,amount, note,note)
+//    }
+//
+//    @Deprecated("displayNoteが設定できない", ReplaceWith("DisplayNoteつき"),DeprecationLevel.WARNING)
+//    fun withdraw(uuid:UUID,amount:Double,note:String):Boolean{
+//        return withdraw(uuid,amount, note,note)
+//    }
 
 }
