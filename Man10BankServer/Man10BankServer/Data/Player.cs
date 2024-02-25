8c60cfd165e8348b6d146ab4c9031824c3d656ba
Jin Morikawa <forest611328@gmail.com>
2024-02-26 07:18:31 +0900

    キャッシュするように

--- Man10BankServer/Man10BankServer/Data/Player.cs
+++ Man10BankServer/Man10BankServer/Data/Player.cs
@@ -1,3 +1,4 @@
+using System.Collections.Concurrent;
 using System.Net;
 using Man10BankServer.Common;
 
@@ -9,6 +10,7 @@ public class Player
     public string Uuid { get; }
 
     private static readonly HttpClient Client = new();
+    private static readonly HashSet<Player> PlayerSet = new();
 
     private Player(string name, string uuid)
     {
@@ -18,23 +20,36 @@ public class Player
 
     public static async Task<Player> GetFromUuid(string uuid)
     {
+        var cachePlayer = PlayerSet.FirstOrDefault(p => p.Uuid == uuid);
+        if (cachePlayer!= null)
+        {
+            return cachePlayer;
+        }
         using var response = await Client.GetAsync($"{Configuration.Man10SystemUrl}/player/mcid?uuid={uuid}");
         if (response.StatusCode == HttpStatusCode.NotFound)
         {
             throw new Exception("プレイヤーが存在しません");
         }
         var body = await response.Content.ReadAsStringAsync();
-        return new Player(body, uuid);
+        var newPlayer = new Player(body, uuid);
+        PlayerSet.Add(newPlayer);
+        return newPlayer;
     } 
     public static async Task<Player> GetFromName(string name)
     {
+        var cachePlayer = PlayerSet.FirstOrDefault(p => p.Name == name);
+        if (cachePlayer!= null)
+        {
+            return cachePlayer;
+        }
         using var response = await Client.GetAsync($"{Configuration.Man10SystemUrl}/player/uuid?minecraftId={name}");
-
         if (response.StatusCode == HttpStatusCode.NotFound)
         {
             throw new Exception("プレイヤーが存在しません");
         }
         var body = await response.Content.ReadAsStringAsync();
-        return new Player(name, body);
+        var newPlayer = new Player(name,body);
+        PlayerSet.Add(newPlayer);
+        return newPlayer;
     }
 }
