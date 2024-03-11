1f56c41588bfc47afe4b55543b06de1fa1a17bf7
Jin Morikawa <forest611328@gmail.com>
2024-03-12 07:00:57 +0900

    API修正

--- Man10Bank/src/main/java/red/man10/man10bank/BankAPI.kt
+++ Man10Bank/src/main/java/red/man10/man10bank/BankAPI.kt
@@ -1,6 +1,9 @@
 package red.man10.man10bank
 
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.bukkit.plugin.java.JavaPlugin
+import red.man10.man10bank.Man10Bank.Companion.coroutineScope
 import red.man10.man10bank.api.APIBank
 import red.man10.man10bank.api.APIServerLoan
 import java.util.*
@@ -8,23 +11,18 @@ import java.util.*
 class BankAPI(private val plugin: JavaPlugin) {
 
     /**
-     * APIサーバーに接続できているかどうか
-     */
-    fun canConnect():Boolean{
-        return Man10Bank.isEnableServer()
-    }
-
-    /**
      * 出金リクエストを送る
      */
-    fun withdraw(uuid: UUID,amount:Double,note:String,displayNote:String):Boolean{
-        val result = APIBank.takeBalance(APIBank.TransactionData(
-            uuid.toString(),
-            amount,
-            plugin.name,
-            note,
-            displayNote
-        ))
+    fun withdraw(uuid: UUID,amount:Double,note:String,displayNote:String):Boolean {
+        val result  = runBlocking {
+            APIBank.takeBalance(APIBank.TransactionData(
+                uuid.toString(),
+                amount,
+                plugin.name,
+                note,
+                displayNote
+            ))
+        }
 
         return result == APIBank.BankResult.Successful
     }
@@ -34,13 +32,15 @@ class BankAPI(private val plugin: JavaPlugin) {
      * @param ver オーバーロードのための仮引数
      */
     fun deposit(uuid: UUID,amount:Double,note:String,displayNote:String,ver:Int = 1):Boolean{
-        val result = APIBank.addBalance(APIBank.TransactionData(
-            uuid.toString(),
-            amount,
-            plugin.name,
-            note,
-            displayNote
-        ))
+        val result = runBlocking {
+            APIBank.addBalance(APIBank.TransactionData(
+                uuid.toString(),
+                amount,
+                plugin.name,
+                note,
+                displayNote
+            ))
+        }
 
         return result == APIBank.BankResult.Successful
     }
@@ -50,61 +50,54 @@ class BankAPI(private val plugin: JavaPlugin) {
      */
     @Deprecated("リターン値のあるバージョンを使用すること")
     fun deposit(uuid: UUID,amount:Double,note:String,displayNote:String){
-        APIBank.addBalance(APIBank.TransactionData(
-            uuid.toString(),
-            amount,
-            plugin.name,
-            note,
-            displayNote
-        ))
+        coroutineScope.launch {
+            APIBank.addBalance(APIBank.TransactionData(
+                uuid.toString(),
+                amount,
+                plugin.name,
+                note,
+                displayNote
+            ))
+        }
     }
 
 
     /**
      * 銀行金額を取得する
      */
-    fun getBank(uuid: UUID):Double{
-        return APIBank.getBalance(uuid)
-    }
+    fun getBank(uuid: UUID):Double = runBlocking { APIBank.getBalance(uuid) }
+
 
     /**
      * リボの金額を取得する
      */
-    fun getServerLoan(uuid: UUID):Double{
-        return APIServerLoan.getInfo(uuid)?.borrow_amount?:0.0
-    }
+    fun getServerLoan(uuid: UUID):Double = runBlocking { APIServerLoan.getInfo(uuid)?.borrow_amount?:0.0 }
 
     /**
      * 非同期で入金する
      * 入金完了するとコールバックが呼ばれる
      */
-    fun asyncDeposit(uuid: UUID,amount: Double,note: String,displayNote: String,callback:(Boolean)->Unit){
-        Man10Bank.threadPool.execute {
-            val ret = deposit(uuid, amount, note, displayNote,1)
-            callback.invoke(ret)
-        }
+    fun asyncDeposit(uuid: UUID,amount: Double,note: String,displayNote: String,callback:(Boolean)->Unit) = coroutineScope.launch {
+        val ret = deposit(uuid, amount, note, displayNote,1)
+        callback.invoke(ret)
     }
 
     /**
      * 非同期で出勤する
      * 出金完了するとコールバックが呼ばれる
      */
-    fun asyncWithdraw(uuid: UUID,amount: Double,note: String,displayNote: String,callback:(Boolean)->Unit){
-        Man10Bank.threadPool.execute {
-            val ret = withdraw(uuid, amount, note, displayNote)
-            callback.invoke(ret)
-        }
+    fun asyncWithdraw(uuid: UUID,amount: Double,note: String,displayNote: String,callback:(Boolean)->Unit) = coroutineScope.launch {
+        val ret = withdraw(uuid, amount, note, displayNote)
+        callback.invoke(ret)
     }
+
 
     /**
      * 非同期で所持金を取得
      * 取得した結果はコールバック関数で取得
      */
-    fun asyncGetBalance(uuid:UUID,callback: (Double)->Unit){
-        Man10Bank.threadPool.execute {
-            callback.invoke(getBank(uuid))
-        }
-    }
+    fun asyncGetBalance(uuid:UUID,callback: (Double)->Unit) = coroutineScope.launch { callback.invoke(getBank(uuid)) }
+
 
 
 //    @Deprecated("displayNoteが設定できない", ReplaceWith("DisplayNoteつき"),DeprecationLevel.WARNING)
