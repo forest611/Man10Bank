aaa597c2635493c6a70bd5f7f6f5da7e893c8697
Jin Morikawa <forest611328@gmail.com>
2025-08-24 15:58:28 +0900

    feat: Estate関連のリポジトリ追加

---
+++ src/main/java/red/man10/man10bank/repository/UserEstateRepository.kt
@@ -0,0 +1,102 @@
+package red.man10.man10bank.repository
+
+import org.ktorm.database.Database
+import org.ktorm.dsl.*
+import red.man10.man10bank.db.tables.EstateTbl
+import java.math.BigDecimal
+
+class UserEstateRepository(private val db: Database) {
+
+    data class EstateParams(
+        val uuid: String,
+        val player: String,
+        val vault: BigDecimal,
+        val bank: BigDecimal,
+        val cash: BigDecimal,
+        val estate: BigDecimal,
+        val loan: BigDecimal,
+        val shop: BigDecimal,
+        val crypto: BigDecimal,
+    ){
+        override fun equals(other: Any?): Boolean {
+            if (other !is EstateParams) return false
+            return uuid == other.uuid &&
+                    player == other.player &&
+                    vault.compareTo(other.vault) == 0 &&
+                    bank.compareTo(other.bank) == 0 &&
+                    cash.compareTo(other.cash) == 0 &&
+                    estate.compareTo(other.estate) == 0 &&
+                    loan.compareTo(other.loan) == 0 &&
+                    shop.compareTo(other.shop) == 0 &&
+                    crypto.compareTo(other.crypto) == 0
+        }
+
+        override fun hashCode(): Int =
+            listOf(
+                uuid, player,
+                vault.stripTrailingZeros().toPlainString(),
+                bank.stripTrailingZeros().toPlainString(),
+                cash.stripTrailingZeros().toPlainString(),
+                estate.stripTrailingZeros().toPlainString(),
+                loan.stripTrailingZeros().toPlainString(),
+                shop.stripTrailingZeros().toPlainString(),
+                crypto.stripTrailingZeros().toPlainString()
+            ).hashCode()
+    }
+
+    //TODO: 成功/失敗/重複の判定をちゃんとする
+    fun addEstateHistory(params: EstateParams): Boolean {
+        if (equalsLastEstate(params)) {
+            return true
+        }
+        val updated = db.update(EstateTbl) {
+            set(it.uuid, params.uuid)
+            set(it.player, params.player)
+            set(it.vault, params.vault)
+            set(it.bank, params.bank)
+            set(it.cash, params.cash)
+            set(it.estate, params.estate)
+            set(it.loan, params.loan)
+            set(it.shop, params.shop)
+            set(it.crypto, params.crypto)
+        }
+        if (updated == 0) {
+            db.insert(EstateTbl) {
+                set(it.uuid, params.uuid)
+                set(it.player, params.player)
+                set(it.vault, params.vault)
+                set(it.bank, params.bank)
+                set(it.cash, params.cash)
+                set(it.estate, params.estate)
+                set(it.loan, params.loan)
+                set(it.shop, params.shop)
+                set(it.crypto, params.crypto)
+            }
+            return true
+        }
+        return false
+    }
+
+    private fun equalsLastEstate(params: EstateParams): Boolean {
+        val lastEstateRecord = db.from(EstateTbl)
+            .select()
+            .where { EstateTbl.uuid eq params.uuid }
+            .orderBy(EstateTbl.id.desc())
+            .limit(1)
+            .map { row ->
+                EstateParams(
+                    uuid = row[EstateTbl.uuid] ?: "",
+                    player = row[EstateTbl.player] ?: "",
+                    vault = row[EstateTbl.vault] ?: BigDecimal.ZERO,
+                    bank = row[EstateTbl.bank] ?: BigDecimal.ZERO,
+                    cash = row[EstateTbl.cash] ?: BigDecimal.ZERO,
+                    estate = row[EstateTbl.estate] ?: BigDecimal.ZERO,
+                    loan = row[EstateTbl.loan] ?: BigDecimal.ZERO,
+                    shop = row[EstateTbl.shop] ?: BigDecimal.ZERO,
+                    crypto = row[EstateTbl.crypto] ?: BigDecimal.ZERO,
+                )
+            }.firstOrNull() ?: return false
+
+        return params == lastEstateRecord
+    }
+}
